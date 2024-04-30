c291c94c364fd0146877fdb9cbe40795c01459ed
Kori <clantxreal@gmail.com>
2024-04-30 14:45:33 -0400

    lock 14key2scratch (1) to 14K2S and clean up its files

--- configs/14key2scratch.config.lua
+++ configs/14key2scratch.config.lua
@@ -5,18 +5,17 @@ local config = JustConfig()
 config.w = 600
 config.h = 600
 local root = (...):match("^(.+)/(.-)/(.-)$")
+local mainConfig = require(root .. "/Modules/configs")
 
 -- note: dont delete the data block comments
 config.data = --[[data]] {
 	accalign = "left",
 	autosave = true,
 	barline = true,
-	ez2dp = false,
 	flipjudges = false,
 	hidemarv = false,
 	hiterrorpos = "up",
 	judgesoffset = 0,
-	layout = "IIDX",
 	litupreceptors = true
 } --[[/data]]
 
@@ -24,20 +23,12 @@ function config:draw(w, h)
 	local data = self.data
 
 	just.indent(15)
-	just.text("14K2S skin configs\n\nPlayfield")
+	just.text("14K2S skin configs")
     imgui.setSize(w, h, w / 2, 55)
-    imgui.separator()
-    data.barline = imgui.checkbox("barline", data.barline, "Show barline")
-    data.layout = imgui.combo("layout", data.layout, {"IIDX", "EZ2AC"}, nil, "Layout style")
-    if data.layout == "IIDX" then data.ez2dp = imgui.checkbox("ez2dp", data.ez2dp, "Use DP layout") end
-    data.litupreceptors = imgui.checkbox("litupreceptors", data.litupreceptors, "Light up receptors")
-    just.text("\n   HUD layout")
-    imgui.separator()
-    data.accalign = imgui.combo("accalign", data.accalign, {"left", "center", "right"}, nil, "Acc display position")
-    data.hiterrorpos = imgui.combo("hiterrorpos", data.hiterrorpos, {"up", "middle", "down"}, nil, "Hit error position")
-    data.judgesoffset = imgui.slider1("judgesoffset", data.judgesoffset, "%.0f", -50, 50, 1, "Combo/Judge pos offset")
-    data.flipjudges = imgui.checkbox("flipjudges", data.flipjudges, "Flip judge/combo elements")
-    data.hidemarv = imgui.checkbox("hidemarv", data.hidemarv, "Tryhard mode")
+    
+    mainConfig.playfieldSettings(data)
+    mainConfig.hudSettings(data)
+
     just.text("\n   Save configs")
     imgui.separator()
 	if imgui.button("Write config file", "Write") then
